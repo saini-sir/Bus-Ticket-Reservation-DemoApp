7f9320685d823cadb5c7c17a99c12cb7e4743abf
saini-sir <sainigsir@gmail.com>
2024-06-20 07:19:54 +0530

    hello changes in my branch

--- src/main/java/com/app/controller/FeedbackController.java
+++ src/main/java/com/app/controller/FeedbackController.java
@@ -52,6 +52,7 @@ public class FeedbackController {
 
 	@PutMapping("/update{key}")
     public ResponseEntity<Feedback> updateRouteById(@Valid @RequestBody Feedback feedback,@PathVariable("key") String key)throws feedbackException, LoginException {
+        String rr = "rajesh";
         System.out.println("hello rajesh");
         int dx = 20;
         return new ResponseEntity<Feedback>(fService.updateFeedBack(feedback,key),HttpStatus.OK);
